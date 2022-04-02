b8d737e286956e22300e20186c246a2c0ed90501
Dennis Schön <mail@dennis-schoen.de>
2022-04-02 19:28:04 +0200

    fix .git ignore syntax

--- nvim/.config/nvim/plugin/telescope_config.lua
+++ nvim/.config/nvim/plugin/telescope_config.lua
@@ -2,7 +2,7 @@ require("telescope").setup {
     defaults = {
         layout_strategy = "vertical",
         file_ignore_patterns = {
-            ".git/.*"
+            "%.git/"
         },
         vimgrep_arguments = {
             "rg",
