7ced9c33ac107530cefae71c7c5b2c83b09d3a85
Dennis Schön <mail@dennis-schoen.de>
2022-04-08 16:02:46 +0200

    more snippets

--- nvim/.config/nvim/luasnippets/go.lua
+++ nvim/.config/nvim/luasnippets/go.lua
@@ -13,4 +13,8 @@ return {
     s("errn", fmt("if err != nil {{\n\treturn nil\n}}\n\n", {} )),
     s("errnw", fmt("if err != nil {{\n\treturn fmt.Errorf(\"{}\", err)\n}}\n\n", { i(0, "message %w") } )),
     s("errt", fmt("if err != nil {{\n\tt.Fatal(err)\n}}\n\n", {} )),
+    s("fori", fmt("for {} := 0; {} < {}; {}++ {{\n\t{}\n}}", { i(1, "i"), rep(1), i(2, "n"), rep(1), i(0) })),
+    s("forr", fmt("for {}, {} := range {} {{\n\t{}\n}}", { i(2, "k"), i(3, "v"), i(1), i(0) })),
+    s("func", fmt("func {}({}) {} {{\n\t{}\n}}", { i(1, "name"), i(2, "params"), i(3), i(0) })),
+    s("qq", fmt("q.Q({})", { i(0) }))
 }
