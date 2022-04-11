18df930f109f82d2a07ccd3eb1902ab0d53335cd
Dennis Schön <mail@dennis-schoen.de>
2022-04-11 12:30:59 +0200

    fix safari development setting

--- set-macos-defaults.sh
+++ set-macos-defaults.sh
@@ -159,8 +159,8 @@ echo "  › Set up Safari for development"
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+defaults write -g WebKitDeveloperExtras -bool true
 
 echo "  › Privacy: don’t send search queries to Apple"
 defaults write com.apple.Safari UniversalSearchEnabled -bool false
