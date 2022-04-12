26071b0ec036b70960f46c53638d9ab47a2d6016
Dennis Schön <mail@dennis-schoen.de>
2022-04-12 09:37:45 +0200

    change telescope layout

--- nvim/.config/nvim/plugin/telescope_config.lua
+++ nvim/.config/nvim/plugin/telescope_config.lua
@@ -1,5 +1,12 @@
 require("telescope").setup {
     defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.8,
+            height = 0.8,
+            preview_height = 0.7,
+            prompt_position = "bottom",
+        },
         file_ignore_patterns = {
             "%.git/"
         },
