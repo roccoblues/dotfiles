6342adb8850ac2bd4b2c531e65e935c6b4ecba93
Dennis Schön <mail@dennis-schoen.de>
2022-05-07 21:38:38 +0200

    add go snippets back

---
+++ nvim/.config/nvim/luasnippets/go.lua
@@ -0,0 +1,41 @@
+local ls = require("luasnip")
+ls.config.set_config({
+  history = true,
+  update_events = "TextChanged,TextChangedI",
+})
+
+local snippet_from_nodes = ls.sn
+local i = ls.insert_node
+local t = ls.text_node
+local d = ls.dynamic_node
+local c = ls.choice_node
+local fmt = require("luasnip.extras.fmt").fmt
+local f = ls.function_node
+local s = ls.snippet
+
+local same = function(index)
+  return f(function(args)
+    return args[1]
+  end, { index })
+end
+
+return {
+  s(":", fmt("{} := {}", { i(1, "v"), i(0, "value") })),
+  s("ap", fmt("append({}, {})", { i(1, "slice"), i(0, "value") })),
+  s("ap=", fmt("{} = append({}, {})", { i(1, "slice"), same(1), i(0, "value") })),
+  s("ife", fmt("if err := {}; err != nil {{\n\t{}\n}}", { i(1, "condition"), i(0) })),
+  s("iota", fmt("const (\n\t{} = iota\n\t{}\n)", { i(1, "name"), i(0) })),
+  s("errn", fmt("if err != nil {{\n\treturn nil\n}}\n\n", {})),
+  s("errnw", fmt("if err != nil {{\n\treturn fmt.Errorf(\"{}\", err)\n}}\n\n", { i(0, "message %w") })),
+  s("errt", fmt("if err != nil {{\n\tt.Fatal(err)\n}}\n\n", {})),
+  s("fori", fmt("for {} := 0; {} < {}; {}++ {{\n\t{}\n}}", { i(1, "i"), same(1), i(2, "n"), same(1), i(0) })),
+  s("forr", fmt("for {}, {} := range {} {{\n\t{}\n}}", { i(2, "k"), i(3, "v"), i(1), i(0) })),
+  s("func", fmt("func {}({}) {} {{\n\t{}\n}}", { i(1, "name"), i(2, "params"), i(3), i(0) })),
+  s("json", fmt("`json:\"{}\"`", { i(0, "key") })),
+  s("qq", fmt("q.Q({})", { i(0) })),
+  s("map", fmt("map[{}]{}", { i(1, "string"), i(0, "int") })),
+  s("select", fmt("select {{\ncase {} := <-{}\n\t{}\n}}", { i(1), i(2, "ch"), i(0) })),
+  s("st", fmt("type {} struct {{\n{}\n}}", { i(1), i(0) })),
+  s("switch", fmt("switch {} {{\ncase {}:\n\t{}\n}}", { i(1, "var"), i(2, "value"), i(0) })),
+  s("test", fmt("func Test{}(t *testing.T) {{\n\t{}\n}}", { i(1, "Function"), i(0) })),
+}
