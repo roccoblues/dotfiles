a45dd0fb2bfc8921eebc75b1703182dfa88ceeb2
Dennis Schön <mail@dennis-schoen.de>
2022-05-30 17:58:45 +0200

    nvim: increase format timeout

--- nvim/.config/nvim/ftplugin/go.lua
+++ nvim/.config/nvim/ftplugin/go.lua
@@ -7,27 +7,14 @@ vim.keymap.set('n', '<leader>T', '<cmd>TestFile<cr>', { silent = true })
 vim.keymap.set('n', '<leader>a', '<cmd>TestSuite<cr>', { silent = true })
 vim.keymap.set('n', 'ga', '<cmd>A<cr>', { silent = true })
 
-function OrgImports(wait_ms)
-  local params = vim.lsp.util.make_range_params()
-  params.context = { only = { "source.organizeImports" } }
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-  for _, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
-      if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
-      else
-        vim.lsp.buf.execute_command(r.command)
-      end
-    end
-  end
-end
+local util = require('util')
 
 local augroup = vim.api.nvim_create_augroup('lsp_buf_format', { clear = true })
 
 vim.api.nvim_create_autocmd('BufWritePre',
-  { pattern = '<buffer>', group = augroup, callback = vim.lsp.buf.formatting_sync }
+  { pattern = '<buffer>', group = augroup, callback = function() vim.lsp.buf.formatting_sync(nil, 3000) end }
 )
 
 vim.api.nvim_create_autocmd('BufWritePre',
-  { pattern = '<buffer>', group = augroup, callback = function() OrgImports(1000) end}
+  { pattern = '<buffer>', group = augroup, callback = function() util.org_imports(3000) end }
 )
---
+++ nvim/.config/nvim/lua/util.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local lsp_util = vim.lsp.util
+
+function M.org_imports(wait_ms)
+  local params = lsp_util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
+
+return M
