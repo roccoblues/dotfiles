4c0c75e809aa04346317672f4058484fe222d6f1
Dennis Schön <mail@dennis-schoen.de>
2022-06-07 12:08:22 +0200

    install asdf plugins

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ fi
 if ! [[ -d ~/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
+
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git || true
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git || true
+asdf plugin-add yarn || true
+asdf plugin-add ruby || true
