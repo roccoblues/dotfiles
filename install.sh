48881a24ef43f0951291cdc12b4fe02bf7f035dc
Dennis Schön <mail@dennis-schoen.de>
2022-07-09 20:34:26 +0200

    don't install tmux-plugins

--- install.sh
+++ install.sh
@@ -8,7 +8,3 @@ done
 if ! [[ -f ~/Library/Application\ Support/Code/User/settings.json ]]; then
     ln -s "$(PWD)/vscode-settings.json" ~/Library/Application\ Support/Code/User/settings.json
 fi
-
-if ! [[ -d ~/.tmux/plugins/tpm ]]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
